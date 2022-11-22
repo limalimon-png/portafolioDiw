f501e1c3dce4ac2f6fe7083c84277521d683705d
guillermo <guillermomartinez1222@gmail.com>
2022-11-22 19:57:06 +0100

    boton

--- js/animacion.js
+++ js/animacion.js
@@ -10,21 +10,33 @@ function minimizarMaximizar(a){
 // //    }
 // card.classList.add("prueba");
 
-a.parentElement.parentElement.parentElement.classList.add("ocultar")
+a.parentElement.parentElement.parentElement.classList.add("ocultarlento")
 a.parentElement.parentElement.parentElement.classList.remove("reaparecer")
 
 // a.parentElement.parentElement.previousSibling.previousSibling.classList.add("reaparecer")
 // a.parentElement.parentElement.previousSibling.previousSibling.classList.remove("ocultar")
-document.getElementById("").parentElement.childNodes
-console.log(a.parentElement.parentElement.parentElement.childNodes);
+// document.getElementById("").parentElement.childNodes
+a.parentElement.parentElement.parentElement.parentElement.children[0].classList.add("reaparecer")
+a.parentElement.parentElement.parentElement.parentElement.children[0].classList.remove("ocultar");
 
    
 }
+function abrir(boton){
+    boton.classList.remove("reaparecer")
+    boton.classList.add("ocultar")
+boton.parentElement.children[1].classList.remove("ocultar")
+boton.parentElement.children[1].classList.add("reaparecer")
 
+}
 // function redireccion(){
 //     // window.location.href="./html/"+htm+".html";
 //     window.location.href = "./html/about.html";
 // }
+
+
+function minimizar(){
+    about=0;
+}
 function comprobarRedireccion(event){
 if(about!=1){
     event.preventDefault();
