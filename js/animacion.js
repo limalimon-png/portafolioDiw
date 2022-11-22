42d99d60f7d34169f2b8df578c90697502277963
guillermo <guillermomartinez1222@gmail.com>
2022-11-22 19:21:06 +0100

    intento animaciones

--- js/animacion.js
+++ js/animacion.js
@@ -1,8 +1,33 @@
+var about=1;
 function minimizarMaximizar(a){
-    let card=a.parentElement.parentElement;
-   if(card.classList[1]=="izquierda"){
+    about=0;
+//     document.getElementById("s").classList.add
+//     let card=a.parentElement.parentElement;
+// //    if(card.classList[1]=="izquierda"){
    
-   }else{
+// //    }else{
 
-   }
+// //    }
+// card.classList.add("prueba");
+
+a.parentElement.parentElement.parentElement.classList.add("ocultar")
+a.parentElement.parentElement.parentElement.classList.remove("reaparecer")
+
+// a.parentElement.parentElement.previousSibling.previousSibling.classList.add("reaparecer")
+// a.parentElement.parentElement.previousSibling.previousSibling.classList.remove("ocultar")
+document.getElementById("").parentElement.childNodes
+console.log(a.parentElement.parentElement.parentElement.childNodes);
+
+   
+}
+
+// function redireccion(){
+//     // window.location.href="./html/"+htm+".html";
+//     window.location.href = "./html/about.html";
+// }
+function comprobarRedireccion(event){
+if(about!=1){
+    event.preventDefault();
+    about=1;
+}
 }
